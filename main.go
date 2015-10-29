c1349f299e8be8d5714855fa0656b2b47b2c66f3
Matheus Felipe <matbhz@gmail.com>
2015-10-28 18:40:32 -0700

    Adds placeholders for endpoints and serve

--- main.go
+++ main.go
@@ -1,6 +1,44 @@
 package main
-import "fmt"
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"fmt"
+	"log"
+)
+
+const GET    = "GET"
+const POST   = "POST"
+const DELETE = "DELETE"
 
 func main() {
-	fmt.Print("Hello world. Golang environment all set!");
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/{topic}", Publish).Methods(POST)
+
+	r.HandleFunc("/api/{topic}/{subscriber}", Subscribe).Methods(POST)
+	r.HandleFunc("/api/{topic}/{subscriber}", RemoveSubscriber).Methods(DELETE)
+	r.HandleFunc("/api/{topic}/{subscriber}", Receive).Methods(GET)
+
+	http.Handle("/", r)
+
+	fmt.Print("Starting server locally on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+
+func Publish(http.ResponseWriter, *http.Request){
+	fmt.Print("POSTed Publish")
+}
+
+func Subscribe(http.ResponseWriter, *http.Request){
+	fmt.Print("POSTed Subscribe")
+}
+
+func RemoveSubscriber(http.ResponseWriter, *http.Request){
+	fmt.Print("DELETEd RemoveSubscriber")
+}
+
+func Receive(http.ResponseWriter, *http.Request){
+	fmt.Print("GET Receive")
 }
